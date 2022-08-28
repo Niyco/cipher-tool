93ec9010d58cbde497a430489e14faa0a7a16251
Niyco <message4oliver@gmail.com>
2022-08-28 12:32:36 +0100

    Added customtkinter library

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import customtkinter as ctk
 import tkinter as tk
 from tkinter import ttk
 
-root = tk.Tk()
+root = ctk.CTk()
 icon = tk.PhotoImage(file=r'resources\icon.png')
+
+ctk.set_appearance_mode('light')
 root.title('Cipher Tool')
 root.geometry('1366x768')
 root.iconphoto(False, icon)
