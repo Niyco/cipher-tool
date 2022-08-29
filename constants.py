6a11d2dd627551e4f8348272812c5fc923e3ea20
Niyco <message4oliver@gmail.com>
2022-08-29 18:00:42 +0100

    Added radio buttons in the toolbar and cleaned up code

--- constants.py
+++ constants.py
@@ -1,17 +1,21 @@
-lang_path = r'lang.json'
+theme_path = r'customtkinter\assets\themes\green.json'
+lang_path = r'lang\lang_en.json'
 icon_path = r'resources\icon.png'
 toolbar_icon_path = r'resources\toolbar.png'
 mode = 'dark'
+theme = 'green'
+radio_selected_color = '#121212'
 default_size ='1366x768'
-min_size = '576x324'
+min_size = '608x342'
 hover_color_change = -20
 toolbar_size = 75
-toolbar_animation_time = 100
+toolbar_step = 5
+toolbar_animation_time = 50
 
 class AnalysisStage:
-    def __init__(self, frame, update): # Create all widgets
+    def __init__(self, frame, update):
         self.update = update
-    def analyse(self, text): # Analyse text and update widget content
+    def analyse(self, text):
         pass
-    def display(self): # Show all widgets (grid/pack/place)
+    def display(self):
         pass
--- main.py
+++ main.py
@@ -4,25 +4,32 @@ import tkinter as tk
 import json
 
 def toolbar_animation(start, stop, step, delay):
+    global toolbar_active
     global toolbar_animated
+    
     toolbar.config(height=start, width=root.winfo_width())
-    if start != stop:
+    if start * (step / abs(step)) < stop:
         toolbar.after(delay, toolbar_animation, start + step, stop, step, delay)
     else:
+        if not toolbar_active:
+            toolbar.place(x=0,y=-1)
         toolbar_animated = False
 
 def toggle_toolbar():
+    global toolbar_active
     global toolbar_animated
+    
     if not toolbar_animated:
-        global toolbar_active
-        
         toolbar_active = not toolbar_active
         if toolbar_active:
+            toolbar.grid(row=0, column=0, columnspan=3)
             toolbar_animated = True
-            toolbar_animation(0, toolbar_size, 1, int(toolbar_animation_time / toolbar_size))
+            toolbar_animation(0, toolbar_size, toolbar_step,
+                              int(toolbar_animation_time / toolbar_size * toolbar_step))
         else:
             toolbar_animated = True
-            toolbar_animation(toolbar_size, 0, -1, int(toolbar_animation_time / toolbar_size))
+            toolbar_animation(toolbar_size, 0, -toolbar_step,
+                              int(toolbar_animation_time / toolbar_size * toolbar_step))
 
 def resize(event):
     if isinstance(event.widget, ctk.windows.ctk_tk.CTk) and not toolbar_animated:
@@ -31,43 +38,69 @@ def resize(event):
         else:
             toolbar.config(height=0, width=event.width)
 
+def adjust(color, amount):
+    new_hex = ''
+    for i in range(2, 8, 2):
+        adjusted = int(color[i - 1:i + 1], 16) + amount
+        new_hex += format(min(max(adjusted, 0), 255), '02X')
+    return '#' + new_hex
+
+def radio_select(button):
+    global radio_selected
+
+    radio_buttons[radio_selected].configure(fg_color=theme['color']['button'][mode_int],
+                                            hover_color=theme['color']['button_hover'][mode_int])
+    radio_buttons[button].configure(fg_color=radio_selected_color, hover_color=radio_selected_color)
+    radio_selected = button
+
 lang_file = open(lang_path)
 lang = json.load(lang_file)
-lang_file.close
+lang_file.close()
+theme_file = open(theme_path)
+theme = json.load(theme_file)
+theme_file.close()
 
 root = ctk.CTk()
-root.bind('<Configure>', resize)
 icon = tk.PhotoImage(file=icon_path)
 toolbar_toggle_image = tk.PhotoImage(file=toolbar_icon_path)
+if mode == 'light': mode_int = 0
+else: mode_int = 1
 
-ctk.set_appearance_mode(mode)
-ctk.set_default_color_theme('green')
 root.title(lang['title'])
-root.geometry(default_size)
-root.minsize(int(min_size.split('x')[0]), int(min_size.split('x')[1]))
 root.iconphoto(False, icon)
+root.geometry(default_size)
+root.bind('<Configure>', resize)
+root.minsize(int(min_size.split('x')[0]), int(min_size.split('x')[1]))
+ctk.set_appearance_mode(mode)
+ctk.set_default_color_theme(theme_path)
 
 toolbar = ctk.CTkFrame(root)
 toolbar.grid_propagate(0)
-toolbar.config(height=0)
-toolbar.grid(row=0, column=0, columnspan=3)
 toolbar_active = False
 toolbar_animated = False
+radio_text = ctk.CTkButton(toolbar, text=lang['radio_text'], height=26,
+                           corner_radius=10, command=lambda: radio_select(0))
+radio_analysis = ctk.CTkButton(toolbar, text=lang['radio_analysis'], height=26,
+                               corner_radius=10, command=lambda: radio_select(1))
+radio_cipher = ctk.CTkButton(toolbar, text=lang['radio_cipher'], height=26,
+                             corner_radius=10, command=lambda: radio_select(2))
+radio_text.place(x=-10, y=0)
+radio_analysis.place(x=-10, y=25)
+radio_cipher.place(x=-10, y=50)
+radio_buttons = [radio_text, radio_analysis, radio_cipher]
+radio_selected = 0
+radio_select(0)
 
-hover_color = '#' + ''.join([format(min(max(int(root['bg'].lstrip('#')[i - 1:i + 1], 16) + hover_color_change, 0), 255), '02X') for i in range(1, 7, 2)])
-toolbar_toggle = ctk.CTkButton(root, text='', image=toolbar_toggle_image, command=toggle_toolbar, width=42, height=30, fg_color=root['bg'], hover_color=hover_color)
-toolbar_toggle.grid(row=1, column=0, padx=5, pady=5, sticky='W')
-
-image1 = tk.PhotoImage(file=r'resources\analysis.png')
-ctk.CTkButton(toolbar, text='', image=image1, width=125, height=25, fg_color=toolbar['bg'], hover_color=toolbar['bg']).grid(row=0, column=0, sticky='W')
-ctk.CTkButton(toolbar, text='Ciphers', height=25).grid(row=1, column=0)
-ctk.CTkButton(toolbar, text='Text', height=25).grid(row=2, column=0)
-
+ctk.CTkButton(root, text='', image=toolbar_toggle_image,
+              width=42, height=30, fg_color=root['bg'],
+              hover_color=adjust(root['bg'], hover_color_change), command=toggle_toolbar
+              ).grid(row=1, column=0, padx=5, pady=5, sticky='W')
 ctk.CTkLabel(root, text=lang['stage_content']).grid(row=1, column=0)
 ctk.CTkLabel(root, text=lang['stage_list']).grid(row=1, column=1)
 ctk.CTkLabel(root, text=lang['output']).grid(row=1, column=2)
 ctk.CTkTextbox(root).grid(row=2, column=0, sticky='NESW')
 ctk.CTkTextbox(root).grid(row=2, column=2, sticky='NESW')
+
 root.columnconfigure(0, weight=1)
 root.columnconfigure(2, weight=1)
 root.rowconfigure(2, weight=1)
