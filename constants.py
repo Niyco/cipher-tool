d42c27dc47cb3a45be074d3122b8ce3de4b0f228
Niyco <message4oliver@gmail.com>
2022-09-02 12:06:15 +0100

    Added a function to enable changing of theme and mode and added new resources

--- constants.py
+++ constants.py
@@ -1,21 +1,20 @@
-theme_path = r'customtkinter\assets\themes\green.json'
-lang_path = r'lang\lang_en.json'
-icon_path = r'resources\icon.png'
-toolbar_icon_path = r'resources\toolbar.png'
-mode = 'dark'
-theme = 'green'
-radio_selected_color = '#121212'
+theme_path = r'themes'
+lang_path = r'lang'
+path_window_icon = r'resources\icon.png'
+path_toolbar_icon = r'resources\toolbar.png'
+path_stage_up = (r'resources\stage_up_light.png', r'resources\stage_up_dark.png')
+path_stage_down = {'green': [r'resources\stage_down_light_1.png', r'resources\stage_down_dark_1.png'],
+                   'blue': [r'resources\stage_down_light_1.png', r'resources\stage_down_dark_1.png']}
+
+color_radio_deselected = ['#BBBBBB', '#4C4C4C']
 default_size ='1366x768'
 min_size = '608x342'
+
 hover_color_change = -20
 toolbar_size = 75
 toolbar_step = 5
 toolbar_animation_time = 50
 
-class AnalysisStage:
-    def __init__(self, frame, update):
-        self.update = update
-    def analyse(self, text):
-        pass
-    def display(self):
-        pass
+mode_name = 'light'
+theme_name = 'green'
+lang_name = 'lang_en'
--- main.py
+++ main.py
@@ -3,32 +3,30 @@ import customtkinter as ctk
 import tkinter as tk
 import json
 
-def toolbar_animation(start, stop, step, delay):
-    global toolbar_active
+def toolbar_animation(toolbar_active, start, stop, step, delay):
     global toolbar_animated
-    
+
     toolbar.config(height=start, width=root.winfo_width())
     if start * (step / abs(step)) < stop:
-        toolbar.after(delay, toolbar_animation, start + step, stop, step, delay)
+        toolbar.after(delay, toolbar_animation, toolbar_active, start + step, stop, step, delay)
     else:
         if not toolbar_active:
-            toolbar.place(x=0,y=-1)
+            toolbar.place(x=0, y=-1)
         toolbar_animated = False
 
 def toggle_toolbar():
-    global toolbar_active
-    global toolbar_animated
+    global toolbar_active, toolbar_animated
     
     if not toolbar_animated:
         toolbar_active = not toolbar_active
         if toolbar_active:
             toolbar.grid(row=0, column=0, columnspan=3)
             toolbar_animated = True
-            toolbar_animation(0, toolbar_size, toolbar_step,
+            toolbar_animation(toolbar_active, 0, toolbar_size, toolbar_step,
                               int(toolbar_animation_time / toolbar_size * toolbar_step))
         else:
             toolbar_animated = True
-            toolbar_animation(toolbar_size, 0, -toolbar_step,
+            toolbar_animation(toolbar_active, toolbar_size, 0, -toolbar_step,
                               int(toolbar_animation_time / toolbar_size * toolbar_step))
 
 def resize(event):
@@ -48,61 +46,85 @@ def adjust(color, amount):
 def radio_select(button):
     global radio_selected
 
-    radio_buttons[radio_selected].configure(fg_color=theme['color']['button'][mode_int],
-                                            hover_color=theme['color']['button_hover'][mode_int])
-    radio_buttons[button].configure(fg_color=radio_selected_color, hover_color=radio_selected_color)
+    radio_buttons[radio_selected].configure(fg_color=color_radio_deselected[mode],
+                                            hover_color=color_radio_deselected[mode])
+    radio_buttons[button].configure(fg_color=theme['color']['button'][mode],
+                                    hover_color=theme['color']['button'][mode])
     radio_selected = button
 
-lang_file = open(lang_path)
-lang = json.load(lang_file)
-lang_file.close()
-theme_file = open(theme_path)
-theme = json.load(theme_file)
-theme_file.close()
+def create_widgets():
+    global toolbar, toolbar_active, tooblar_animated, radio_buttons, radio_selected, stages_canvas
 
+    toolbar_active = False
+    toolbar_animated = False
+    radio_selected = 0
+    
+    toolbar = ctk.CTkFrame(root)
+    toolbar.grid_propagate(0)
+    radio_text = ctk.CTkButton(toolbar, text=lang['radio_text'], height=26,
+                               corner_radius=10, command=lambda: radio_select(0),
+                               fg_color=color_radio_deselected[mode], hover=False)
+    radio_analysis = ctk.CTkButton(toolbar, text=lang['radio_analysis'], height=26,
+                                   corner_radius=10, command=lambda: radio_select(1),
+                                   fg_color=color_radio_deselected[mode], hover=False)
+    radio_cipher = ctk.CTkButton(toolbar, text=lang['radio_cipher'], height=26,
+                                 corner_radius=10, command=lambda: radio_select(2),
+                                 fg_color=color_radio_deselected[mode], hover=False)
+    radio_text.place(x=-10, y=0)
+    radio_analysis.place(x=-10, y=25)
+    radio_cipher.place(x=-10, y=50)
+    radio_buttons = [radio_text, radio_analysis, radio_cipher]
+    stages_canvas = ctk.CTkCanvas(root, bg=['#EBEBEC', '#212325'][mode], highlightthickness=0)
+    stages_canvas.columnconfigure(0, weight=1)
+    stages_canvas.grid(row=2, column=1, sticky='NESW')
+
+    ctk.CTkButton(root, text='', image=toolbar_toggle_image,
+                  width=42, height=30, fg_color=root['bg'],
+                  hover_color=adjust(root['bg'], hover_color_change), command=toggle_toolbar
+                  ).grid(row=1, column=0, padx=5, pady=5, sticky='W')
+    ctk.CTkLabel(root, text=lang['stage_content']).grid(row=1, column=0)
+    ctk.CTkLabel(root, text=lang['stage_list']).grid(row=1, column=1)
+    ctk.CTkLabel(root, text=lang['output']).grid(row=1, column=2)
+    ctk.CTkTextbox(root).grid(row=2, column=0, sticky='NESW')
+    ctk.CTkTextbox(root).grid(row=2, column=2, sticky='NESW')
+
+def update_window():
+    global lang, theme, mode, stage_up_image, stage_down_image
+    
+    lang_file = open(lang_path + '\\' + lang_name + '.json')
+    lang = json.load(lang_file)
+    lang_file.close()
+    theme_file = open(theme_path + '\\' + theme_name + '.json')
+    theme = json.load(theme_file)
+    theme_file.close()
+    
+    if mode_name == 'light': mode = 0
+    else: mode = 1
+    stage_up_image = tk.PhotoImage(file=path_stage_up[mode])
+    stage_down_image = tk.PhotoImage(file=path_stage_down[theme_name][mode])
+    root.title(lang['title'])
+
+    ctk.set_appearance_mode(mode_name)
+    ctk.set_default_color_theme(theme_path + '\\' + theme_name + '.json')
+
+    for widget in root.winfo_children():
+        widget.destroy()
+    create_widgets()
+    
 root = ctk.CTk()
-icon = tk.PhotoImage(file=icon_path)
-toolbar_toggle_image = tk.PhotoImage(file=toolbar_icon_path)
-if mode == 'light': mode_int = 0
-else: mode_int = 1
+icon = tk.PhotoImage(file=path_window_icon)
+toolbar_toggle_image = tk.PhotoImage(file=path_toolbar_icon)
+toolbar_animated = False
 
-root.title(lang['title'])
 root.iconphoto(False, icon)
 root.geometry(default_size)
 root.bind('<Configure>', resize)
 root.minsize(int(min_size.split('x')[0]), int(min_size.split('x')[1]))
-ctk.set_appearance_mode(mode)
-ctk.set_default_color_theme(theme_path)
-
-toolbar = ctk.CTkFrame(root)
-toolbar.grid_propagate(0)
-toolbar_active = False
-toolbar_animated = False
-radio_text = ctk.CTkButton(toolbar, text=lang['radio_text'], height=26,
-                           corner_radius=10, command=lambda: radio_select(0))
-radio_analysis = ctk.CTkButton(toolbar, text=lang['radio_analysis'], height=26,
-                               corner_radius=10, command=lambda: radio_select(1))
-radio_cipher = ctk.CTkButton(toolbar, text=lang['radio_cipher'], height=26,
-                             corner_radius=10, command=lambda: radio_select(2))
-radio_text.place(x=-10, y=0)
-radio_analysis.place(x=-10, y=25)
-radio_cipher.place(x=-10, y=50)
-radio_buttons = [radio_text, radio_analysis, radio_cipher]
-radio_selected = 0
-radio_select(0)
-
-ctk.CTkButton(root, text='', image=toolbar_toggle_image,
-              width=42, height=30, fg_color=root['bg'],
-              hover_color=adjust(root['bg'], hover_color_change), command=toggle_toolbar
-              ).grid(row=1, column=0, padx=5, pady=5, sticky='W')
-ctk.CTkLabel(root, text=lang['stage_content']).grid(row=1, column=0)
-ctk.CTkLabel(root, text=lang['stage_list']).grid(row=1, column=1)
-ctk.CTkLabel(root, text=lang['output']).grid(row=1, column=2)
-ctk.CTkTextbox(root).grid(row=2, column=0, sticky='NESW')
-ctk.CTkTextbox(root).grid(row=2, column=2, sticky='NESW')
-
 root.columnconfigure(0, weight=1)
 root.columnconfigure(2, weight=1)
 root.rowconfigure(2, weight=1)
+
+update_window()
+radio_select(0)
 
 root.mainloop()
