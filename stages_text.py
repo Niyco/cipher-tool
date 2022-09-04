a83eb51edd1e0498f4babf8a9ff14f0f1a4fe288
Niyco <message4oliver@gmail.com>
2022-09-04 17:09:34 +0100

    Added forgotten files

---
+++ stages_text.py
@@ -0,0 +1,12 @@
+class UpperCase:
+    def __init__(self, frame, update_ouput):
+        pass
+    
+    def setup(self, frame):
+        pass
+        
+    def update(self, text):
+        return text.upper()
+    
+    def display(self):
+        pass
